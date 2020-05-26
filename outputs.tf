b769b9c29dc5184559bdb8aaf41aad931d0f15cd
Steven Black <genseb7@gmail.com>
2020-05-25 22:10:46 -0400

    sort outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
+output "address" {
+  value = aws_db_instance.this.address
+}
+
+output "instance" {
+  value = {
+    address  = aws_db_instance.this.address
+    id       = aws_db_instance.this.id
+    username = aws_db_instance.this.username
+  }
+}
+
+output "instance-id" {
+  value = aws_db_instance.this.id
+}
+
 output "password-arn" {
   value = aws_secretsmanager_secret.password.arn
 }
@@ -6,22 +22,6 @@ output "password-version" {
   value = aws_secretsmanager_secret_version.password_val.version_id
 }
 
-output "instance" {
-  value = {
-    id       = aws_db_instance.this.id
-    username = aws_db_instance.this.username
-    address  = aws_db_instance.this.address
-  }
-}
-
-output "instance-id" {
-  value = aws_db_instance.this.id
-}
-
 output "username" {
   value = aws_db_instance.this.username
-}
-
-output "address" {
-  value = aws_db_instance.this.address
 }
